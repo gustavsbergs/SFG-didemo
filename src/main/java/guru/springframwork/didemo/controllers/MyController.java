4377dac5bd2871813146ede494014a4c48774cb9
telumindel <42297556+telumindel@users.noreply.github.com>
2018-11-06 17:14:00 +0200

    Added controller for hello world application

--- src/main/java/guru/springframwork/didemo/controllers/MyController.java
+++ src/main/java/guru/springframwork/didemo/controllers/MyController.java
@@ -1,4 +1,16 @@
 package guru.springframwork.didemo.controllers;
 
+import guru.springframwork.didemo.DiDemoApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+
+import java.applet.AppletContext;
+
+@Controller
 public class MyController {
+    public String hello(){
+       System.out.println("Hello!");
+
+       return "return";
+    }
 }
