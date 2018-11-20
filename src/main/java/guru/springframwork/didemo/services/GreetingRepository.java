80fca283eac00d4188b8cbfbdef41cf862ab060d
telumindel <42297556+telumindel@users.noreply.github.com>
2018-11-20 14:42:24 +0200

    Adding beanfactory configuration

---
+++ src/main/java/guru/springframwork/didemo/services/GreetingRepository.java
@@ -0,0 +1,7 @@
+package guru.springframwork.didemo.services;
+
+public interface GreetingRepository {
+    String getEnglishGreeting();
+    String getSpanishGreeting();
+    String getGermanGreeting();
+}
--- src/main/java/guru/springframwork/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframwork/didemo/services/PrimaryGermanGreetingService.java
@@ -1,14 +1,16 @@
 package guru.springframwork.didemo.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Service
-@Primary
-@Profile("de")
+
+
 public class PrimaryGermanGreetingService implements GreetingService {
+    private GreetingRepository greetingRepository;
+
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository){
+        this.greetingRepository = greetingRepository;
+    }
+    @Override
     public String sayGreeting(){
-        return "Primarer Deutsches hallo!";
+        return greetingRepository.getGermanGreeting();
     }
 }
