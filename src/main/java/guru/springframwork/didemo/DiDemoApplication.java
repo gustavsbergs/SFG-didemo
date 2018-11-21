c92640c89d3e3ed5f59d21da858df475a7bb4871
telumindel <42297556+telumindel@users.noreply.github.com>
2018-11-21 13:59:49 +0200

    Testing environment properties and external properties

--- src/main/java/guru/springframwork/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframwork/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import guru.springframwork.didemo.controllers.ConstructorInjectedController;
 import guru.springframwork.didemo.controllers.MyController;
 import guru.springframwork.didemo.controllers.PropertyInjectedController;
 import guru.springframwork.didemo.controllers.GetterInjectedController;
+import guru.springframwork.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +16,7 @@ public class DiDemoApplication {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-    	System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
 	}
 }
