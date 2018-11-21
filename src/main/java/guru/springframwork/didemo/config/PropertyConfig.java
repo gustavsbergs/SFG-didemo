bebd7e511e08ece8ae2f1c052084f1b50486ddc5
telumindel <42297556+telumindel@users.noreply.github.com>
2018-11-21 14:21:12 +0200

    Refactoring to use application.properties instead of custom property files

--- src/main/java/guru/springframwork/didemo/config/PropertyConfig.java
+++ src/main/java/guru/springframwork/didemo/config/PropertyConfig.java
@@ -2,23 +2,14 @@ package guru.springframwork.didemo.config;
 
 import guru.springframwork.didemo.examplebeans.FakeDataSource;
 import guru.springframwork.didemo.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-    @Autowired
-    Environment env;
+
     @Value("${guru.username}")
     String user;
     @Value("${guru.password}")
@@ -35,7 +26,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -48,9 +39,6 @@ public class PropertyConfig {
         fakeJmsBroker.setUrl(jmsUrl);
         return fakeJmsBroker;
     }
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
+
+
 }
